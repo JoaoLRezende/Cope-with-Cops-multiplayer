2c8eeb06f07048d3e3fd7015124775a0e659181e
jplrez <joaozinhobent@gmail.com>
2019-10-03 20:53:40 -0300

    Update readme and add some constants to be used later

--- common/constants.py
+++ common/constants.py
@@ -1,4 +1,4 @@
-## configurable values ##
+### configurable values ###
 
 MIN_SCREEN_HEIGHT = 40
 ROAD_WIDTH = 130
@@ -8,6 +8,12 @@ CAR_WIDTH  = 2
 
 PLAYER_DISTANCE_FROM_BOTTOM = 10
 
-## derived constants ##
+MAX_FPS = 15
+
+
+### derived constants ###
 
 MIN_SCREEN_WIDTH = ROAD_WIDTH + 2
+
+# The minimum amount of time that must pass between ticks, in milliseconds. (#TODO: enforce this.)
+MIN_TICK_INTERVAL = round(1 / MAX_FPS * 1000)
